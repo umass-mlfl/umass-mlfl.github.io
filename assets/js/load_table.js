06d6b89179a8626717ef36f1329fe3f79b16a856
Neha Kennard <nnayak@cs.umass.edu>
2020-01-15 12:38:15 -0500

    neatening up javascript

--- assets/js/load_table.js
+++ assets/js/load_table.js
@@ -1,105 +1,121 @@
-function format ( d ) {
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
-        '<tr>'+
-            '<td>Bio:</td>'+
-            '<td>'+d.bio+'</td>'+
-        '</tr>'+
-        '<tr>'+
-            '<td>Abstract:</td>'+
-            '<td>'+d.abstract+'</td>'+
-        '</tr>'+
+function format(d) {
+    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
+        '<tr>' +
+        '<td>Bio:</td>' +
+        '<td>' + d.bio + '</td>' +
+        '</tr>' +
+        '<tr>' +
+        '<td>Abstract:</td>' +
+        '<td>' + d.abstract + '</td>' +
+        '</tr>' +
         '</table>';
 }
 
 $(document).ready(function() {
-    var abstract_table = $('#abstracts').DataTable( {
-        paging:   false,
+    var abstract_table = $('#abstracts').DataTable({
+        paging: false,
         ordering: false,
-        info:     false,
+        info: false,
         ajax: "../../archive.txt",
-        columns: [
-					 {
-                "className":      'details-control',
-                "orderable":      false,
-                "data":           null,
-                "defaultContent": ''
-            },
-            { title: "Speaker", data: "speaker",
-                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-            if (oData["website"]){
-              $(nTd).html("<a href='" + oData["website"] + "'> " + sData + " </a>");
-            } else {
-              $(nTd).html(sData);
+        columns: [{
+            "className": 'details-control',
+            "orderable": false,
+            "data": null,
+            "defaultContent": ''
+        }, {
+            title: "Speaker",
+            data: "speaker",
+            "fnCreatedCell": function(nTd, sData, oData, iRow, iCol) {
+                if (oData["website"]) {
+                    $(nTd).html("<a href='" + oData["website"] + "'> " + sData + " </a>");
+                } else {
+                    $(nTd).html(sData);
+                }
             }
-            }},
-            { data: "title", title: "Title" },
-            { data: "date", title: "Date" },
-            { data: "area", title: "Research area" }, 
-            { title: "Video", data: "video",
-                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-            if (sData) {
-            $(nTd).html("<a href='" + sData + "'> Video </a>");
-            } else {
-            $(nTd).html("-");
+        }, {
+            data: "title",
+            title: "Title"
+        }, {
+            data: "date",
+            title: "Date"
+        }, {
+            data: "area",
+            title: "Research area"
+        }, {
+            title: "Video",
+            data: "video",
+            "fnCreatedCell": function(nTd, sData, oData, iRow, iCol) {
+                if (sData) {
+                    $(nTd).html("<a href='" + sData + "'> Video </a>");
+                } else {
+                    $(nTd).html("-");
+                }
             }
-          }},
-        ]
-    } );
+        }, ]
+    });
 
 
-$('#abstracts tbody').on('click', 'td.details-control', function () {
+    $('#abstracts tbody').on('click', 'td.details-control', function() {
         var tr = $(this).closest('tr');
-        var row = abstract_table.row( tr );
- 
-        if ( row.child.isShown() ) {
+        var row = abstract_table.row(tr);
+
+        if (row.child.isShown()) {
             // This row is already open - close it
             row.child.hide();
             tr.removeClass('shown');
-        }
-        else {
+        } else {
             // Open this row
-            row.child( format(row.data()) ).show();
+            row.child(format(row.data())).show();
             tr.addClass('shown');
         }
-    } );
+    });
 
-} );
+});
 $(document).ready(function() {
-    var schedule_table = $('#schedule').DataTable( {
-        paging:   false,
-        info:     false,
+    var schedule_table = $('#schedule').DataTable({
+        paging: false,
+        info: false,
         bFilter: false,
         ajax: "../../this_semester.txt",
-        order: [[ 2, "asc" ]],
-        columns: [
-          { title: "", data: "key",
-                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-            if (sData) {
-            $(nTd).html("<img src='/assets/headshots/" + oData["key"] + ".jpg' width=150 onerror=\"this.onerror=null; this.src='/assets/images/MLFL.png'\"/>");
-            } else {
-            $(nTd).html("-");
-            }
-          }},
-            { title: "Speaker", data: "speaker",
-                "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-            if (oData["website"]){
-              $(nTd).html("<a href='" + oData["website"] + "'> " + sData + " </a>");
-            } else {
-              $(nTd).html(sData);
-            }
-            }},
-            { title: "Date", mRender: function (data, type, full) {
-              console.log(data);
-              console.log(full);
-    if(type == 'sort') return full.date;
-    return full.prettyDate
-  }
-          },
-            { data: "title", title: "Title"},
-            { data: "area", title: "Research area" }, 
+        order: [
+            [2, "asc"]
         ],
-      columnDefs: [{ orderable: false, targets: '_all'}]
-    } );
-} );
-
-
+        columns: [{
+            title: "",
+            data: "key",
+            "fnCreatedCell": function(nTd, sData, oData, iRow, iCol) {
+                if (sData) {
+                    $(nTd).html("<img src='/assets/headshots/" + oData["key"] + ".jpg' width=150 onerror=\"this.onerror=null; this.src='/assets/images/MLFL.png'\"/>");
+                } else {
+                    $(nTd).html("-");
+                }
+            }
+        }, {
+            title: "Speaker",
+            data: "speaker",
+            "fnCreatedCell": function(nTd, sData, oData, iRow, iCol) {
+                if (oData["website"]) {
+                    $(nTd).html("<a href='" + oData["website"] + "'> " + sData + " </a>");
+                } else {
+                    $(nTd).html(sData);
+                }
+            }
+        }, {
+            title: "Date",
+            mRender: function(data, type, full) {
+                if (type == 'sort') return full.date;
+                return full.prettyDate
+            }
+        }, {
+            data: "title",
+            title: "Title"
+        }, {
+            data: "area",
+            title: "Research area"
+        }, ],
+        columnDefs: [{
+            orderable: false,
+            targets: '_all'
+        }]
+    });
+});
